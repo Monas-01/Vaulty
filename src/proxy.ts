9bfb3b9323379d5eaaa600e6fc1928e1e9c83619
Monas-01 <your-github-email@example.com>
2026-09-01 23:18:17 +0500

    clerk production issue fixed

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,12 +1,17 @@
 import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default clerkMiddleware()
+export default clerkMiddleware({
+  frontendApiProxy: {
+    enabled: true,
+  },
+})
 
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for Clerk's auto-proxy path
-    '/__clerk/:path*',
+    // Always run for Clerk's Frontend API proxy
+    '/__clerk/(.*)',
+    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 }
